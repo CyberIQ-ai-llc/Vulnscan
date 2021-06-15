f66274a2d54698013d94ae513e6f784c9275e23d
GorlockStevensen <78938163+GorlockStevensen@users.noreply.github.com>
2021-06-15 15:34:10 -0400

    Update vulnscan.sh
    major change

--- vulnscan.sh
+++ vulnscan.sh
@@ -1,116 +1,251 @@
 #!/bin/bash
 
-##### This script is a companion script to vulnscan.py and will not run on it's own
-# NOTE: the 'directory' variable can be set to any value you want as a default
-# or set to a blank string ''
-# This is a script to do an nmap scan with vulners and render it in a browser.
+# In each of the case statements you have the option of setting a default choice or selecting a choice every
+# time. You will probably find that you use the same choices most of the time. To switch to having
+# a default, for each case statement that you want to have a default, uncomment the two lines following the 
+# while statement, set the number value for the choice variable (no spaces) and comment out the following read
+# prompt. This was initially for testing but found them useful for general use or for repeating a monthly scan.
+# There were some intermitten errors with using Edge Chromium browser as the default browser, If you are 
+# experencing issues at the end of the script uncomment:
+#"#sudo -u $username firefox saved_nmap_scans/$directory/$filename.html&"
+# and comment out: 
+#"sudo -u $username xdg-open saved_nmap_scans/$directory/$filename.html&"
+# Running in a terminal in VS Code has the added benefit of allowing to quickly view the files in the code editor.
 
-# (1) 'IP' or $IP when used as a variable gets the network address from vulnscan.py
-# 'username' gets the non-root username from  vulnscan.py.
 
-IP=$1
+ip=$1
 username=$2
-# A couple color vars
+declare -i oops=0
 
-cg='\e[92m'  # color green
-cb='\e[34m'  # color blue
-cy='\e[93m'  # color yellow
-cn='\e[0m'   # color none
+cg='\e[92m'
+cb='\e[34m'
+cy='\e[93m'
+cn='\e[0m'
 
-# (2) This section variable will create a directory to store the output of the scans.
-# It takes user input to name the 'directory' variable then gives the 'filename'
-# variable the same value as 'directory'.  It then appends the date/time to the value
-# of the 'directory' to make sure the directory name is unique an to know when it was 
-# created. finally it creates a directory in 'saved_nmap_scans'.
+directory='nmap_'    # Edit to change your default
 
-directory='ATT_home_'
+clear
+
+echo -e "\n${cy}Your current scan: ${cg}sudo nmap -Pn -sS $ip${cn}"
 echo -e "\nEnter a directory name you want to save results to"
 echo -e "$cy[Enter]$cn to use default or backspace and enter a new name:"
 read -e -i $directory -p ">> " directory
 
 filename=$directory
-directory=$directory$(date +%Y%m%d_%H%M%S)
+directory=$directory$(date +%m_%d_%Y_%H:%M:%S)
 mkdir saved_nmap_scans/$directory
 
-# (3) Select ports to be scanned or use defaults.
-# NOTE: Feel free to modify the default ports.
-# I didn't bother to trap for inalid entries possibly in the future I might try
-# doing some error handling for 'ports' but that would be hard since there are 
-# so many possible combinations
+clear
 
-ports='1-65535'
-echo -e "$cg\nProper formats for entering ports look like the following:"
-echo -e "22,53,80,443  NOTE: no spaces after the commas"
-echo -e "1-1024 or 1-1024,1723,3389$cn\n"
-echo -e "Do you want to use the default of all ports 1-65535."
-echo -e "$cy(Entering ANY value other than 'n' will result in the default of all ports)$cn"
-echo -e "$cy[Enter]$cn to use the default or $cy'n'$cn to select different ports:"
-read -p ">> " yorn
+echo -e "\n${cy}Your current scan:${cn} sudo nmap -Pn -sS ${cg}-oA saved_nmap_scans/$directory/$filename${cn} $ip"
 
-if [[ $yorn == 'n' ]]; then
-    echo -e "\nEnter the port(s) or ports range: "
-    read -p ">> " ports
-fi
+echo -e "\nSet ports to be scanned"
+echo -e "\t1) Top 1000........(Default if no ports given)" 
+echo -e "\t2) Top 100.........(-F)" 
+echo -e "\t3) All 65535 ports.(-p-)"
+echo -e "\t4) Select ports to scan"
 
-# (4) This section allows the user to set the Timing of the scan.
-
-speed=6  # Needs to be set to a number out of the range of the below while statement.
-while [[ $speed -lt 0 || $speed -gt 5 ]]; do
-    speed=4
-    echo -e "\n$cy(Slower speed may result in more accurate results)$cn"
-    echo -e "Select the speed of the scan integers 0-5:"
-    read -e -i $speed -p ">> " speed
+while [[ $oops == 0 ]]; do
+    #choice=3                                 
+    #read -e -i $choice -p ">>> " choice
+    read -p ">>> " choice
+    case $choice in
+        1) ports=''
+           oops=1
+           ;;
+        2) ports='-F'
+           oops=1
+           ;;
+        3) ports="-p-"
+           oops=1
+           ;;
+        4) echo -e "Enter the port/s you wish to scan seperated by commas no spaces or a range like 1-1024"
+           read -e -p '>>> ' ports
+           ports='-p'$ports
+           oops=1
+           ;;
+        *) echo -e "Invalid input please enter 1-4"
+           ;;
+    esac      
 done
 
-# (5) This is a host discovery scan.  The nmap_grep.txt file is overwritten each time the script is run
+clear
 
-sudo nmap -v -sn -PU T4 -oG nmap_grep.txt $IP   # I switched out -PU fo this --disable-arp-ping
+echo -e "\n${cy}Your current scan:${cn} sudo nmap ${cg}$ports${cn} -Pn -sS -oA saved_nmap_scans/$directory/$filename $ip"
 
-# (6) This line filters out the 'Up' 'Hosts' and checks for unique values then 
-# saves them to 'up_hosts.txt' in the created 'directory'.
+oops=0
+echo -e "\nSet scan timing"
+echo -e "\t0) Paranoid...(-T0)"
+echo -e "\t1) Sneaky.....(-T1)"
+echo -e "\t2) Polite.....(-T2)"
+echo -e "\t3) Normal.....(-T3)"
+echo -e "\t4) Agressive..(-T4)"
+echo -e "\t5) Insane.....(-T5)" 
 
-cat nmap_grep.txt | grep 'Host' | grep 'Up' | uniq > saved_nmap_scans/$directory/up_hosts.txt
+while [[ $oops == 0 ]]; do
+    #choice=4                                 
+    #read -e -i $choice -p ">>> " choice
+    read -p ">>> " choice
+    case $choice in
+        0) speed='-T0'
+           oops=1
+           ;;
+        1) speed='-T1'
+           oops=1
+           ;;
+        2) speed='-T2'
+           oops=1
+           ;;
+        3) speed='-T3'
+           oops=1
+           ;;
+        4) speed='-T4'
+           oops=1
+           ;;
+        5) speed='-T5'
+           oops=1
+           ;;
+        *) echo -e "Invalid input please enter 0-5"
+           ;;
+    esac      
+done
 
-# (7) This line filters out the IPs in nmap_grep.txt and checks for unique values then 
-# puts them in the input_list.txt file
-# The input_list.txt file will be overwritten on each run of the script.
+clear
 
-cat nmap_grep.txt | grep 'Host' | grep 'Up' | awk '{ print $2 }' | uniq > input_list.txt
+echo -e "\n${cy}Your current scan:${cn} sudo nmap $ports ${cg}$speed${cn} -Pn -sS -oA saved_nmap_scans/$directory/$filename $ip"
 
-# (8) This line does an indepth nmap scan doing service discovery(-sV), and runs the vulners
-# script.  It also saves the results (-oG and -oN)to chosen filename in the created directory
-# and an xml file scanfile.xml. 
+oops=0
+echo -e "\nEnter verbosity level"
+echo -e "\t0) Silent (-q)"
+echo -e "\t1) -v"
+echo -e "\t2) -vv"
+echo -e "\t3) -vvv"
+echo -e "\t4) -vvvv"
+echo -e "\t5) Default verbosity"
 
-nmap --script=default,vulners.nse -v -Pn -sV -T$speed -iL input_list.txt -oX scanfile.xml -oG saved_nmap_scans/$directory/$filename.grep -oN saved_nmap_scans/$directory/$filename.txt -p$ports $IP  ## $port ,vulscan --script-args vulscandb=exploitdb.csv 
+while [[ $oops == 0 ]]; do
+    #choice=3                                 
+    #read -e -i $choice -p ">>> " choice
+    read -p ">>> " choice
+    case $choice in
+        0) verbosity='-q'
+           oops=1
+           ;;
+        1) verbosity='-v'
+           oops=1
+           ;;
+        2) verbosity='-vv'
+           oops=1
+           ;;
+        3) verbosity='-vvv'
+           oops=1
+           ;;
+        4) verbosity='-vvvv'
+           oops=1
+           ;;
+        5) verbosity=''
+           oops=1
+           ;;
+        *) echo -e "Invalid input please enter 0-5"
+           ;;
+    esac      
+done
 
-# (9) The xsltproc process creates an .html version of scanfile.xml both of these files will be overwritten
-# the next time the script is run.
+clear
 
-xsltproc scanfile.xml -o scanfile.html
+echo -e "\n${cy}Your current scan:${cn} sudo nmap $ports $speed ${cg}$verbosity${cn} -Pn -sS -oA saved_nmap_scans/$directory/$filename $ip"
 
-# (10) The next two lines create empty files of the chosen filename with the .xml and .html extensions 
+echo -e "\nSet service and OS discovery"
+echo -e "\t0) No service or OS discovery"
+echo -e "\t1) Service discovery only........(-sV)"
+echo -e "\t2) OS discovery only.............(-O)"
+echo -e "\t3) Both service and OS discovery.(-sV -0)"
 
-touch saved_nmap_scans/$directory/$filename.xml
-touch saved_nmap_scans/$directory/$filename.html
+oops=0
+while [[ $oops == 0 ]]; do
+    #choice=4                                 
+    #read -e -i $choice -p ">>> " choice
+    read -p ">>> " choice
+    case $choice in
+        0) disc=''
+           oops=1
+           ;;
+        1) disc='-sV'
+           oops=1
+           ;;
+        2) disc='-O'
+           oops=1
+           ;;
+        3) disc='-sV -O'
+           oops=1
+           ;;
+        *) echo -e "Invalid input please enter 0-3"
+           ;;
+    esac      
+done
 
-# (11) The next two lines copy the contents of scanfile.xml and scanfile.html to the files created above.
+clear
 
-cp scanfile.xml  saved_nmap_scans/$directory/$filename.xml
-cp scanfile.html saved_nmap_scans/$directory/$filename.html
+echo -e "\n${cy}Your current scan:${cn} sudo nmap $ports $speed $verbosity ${cg}$disc${cn} -Pn -sS -oA saved_nmap_scans/$directory/$filename $ip"
 
-# (12) This line takes the script out of root user by using the value of $username passed from  
-# 'vulscan.py' then opens the scanfile.html in the default browser.
+echo -e "\nSet nmap scripting engines to run"
+echo -e "\t0) None"
+echo -e "\t1) default only....(--script=default or -sC)"
+echo -e "\t2) vuln only.......(--script=vuln)"
+echo -e "\t3) vulners only....(--script=vulners.nse)"
+echo -e "\t4) Run all of them.(--script=default,vuln,vulners.nse)"
 
-sudo -u $username xdg-open scanfile.html
+oops=0
+while [[ $oops == 0 ]]; do
+    #choice=4                                 
+    #read -e -i $choice -p ">>> " choice
+    read -p ">>> " choice
+    case $choice in
+        0) script=''
+           oops=1
+           ;;
+        1) script='--script=default'
+           oops=1
+           ;;
+        2) script='--script=vuln'
+           oops=1
+           ;;
+        3) script='--script=vulners.nse'
+           oops=1
+           ;;
+        4) script='--script=default,vuln,vulners.nse'
+           oops=1
+           ;;
+        *) echo -e "Invalid input please enter 0-4"
+           ;;
+    esac      
+done
 
-# (13) Prints to the screen the location of the output files.
+clear
+
+scan="$script $ports $speed $verbosity $disc -Pn -sS -iL saved_nmap_scans/$directory/input_list.txt -oA saved_nmap_scans/$directory/$filename $ip"
+# echo -e "$scan" >> test.txt
+echo -e "\nThis script will generate an input list during host discovery which will be added to your scan\n"
+echo -e "\n${cy}Your current scan:${cn} sudo nmap ${cg}$script${cn} $ports $speed $verbosity $disc -Pn ${cg}-iL saved_nmap_scans/$directory/input_list.txt${cn} -oA saved_nmap_scans/$directory/$filename $ip"
+echo -e "\n${cb}You can press [?] during scan to view Interactive keyboard commands:${cn}\n"
+read -e -p "Press [Enter] to continue" pause
+
+sudo nmap -sn -PA -T4 -oG saved_nmap_scans/$directory/$filename.gnmap $ip
+cat saved_nmap_scans/$directory/$filename.gnmap | grep 'Host' | grep 'Up' | uniq > saved_nmap_scans/$directory/up_hosts.txt
+cat saved_nmap_scans/$directory/up_hosts.txt | grep 'Host' | awk '{ print $2 }' | uniq > saved_nmap_scans/$directory/input_list.txt
+sudo rm -r saved_nmap_scans/$directory/$filename.gnmap
+
+sudo nmap $scan
+
+xsltproc saved_nmap_scans/$directory/$filename.xml -o saved_nmap_scans/$directory/$filename.html
+
+sudo -u $username xdg-open saved_nmap_scans/$directory/$filename.html&
+#sudo -u $username firefox saved_nmap_scans/$directory/$filename.html&
 
 echo -e "\nThe following reports are available at:"
-echo -e "\n  ~/vulnscan/saved_nmap_scans/$directory/$filename.xml"
-echo -e "  ~/vulnscan/saved_nmap_scans/$directory/$filename.html"
-echo -e "  ~/vulnscan/saved_nmap_scans/$directory/$filename.txt"
-echo -e "  ~/vulnscan/saved_nmap_scans/$directory/$filename.grep"
-echo -e "  ~/vulnscan/saved_nmap_scans/$directory/up_hosts.txt\n"
-
-
-
+echo -e "\n  ${cg}~/scan_builder2/saved_nmap_scans/$directory/$filename.nmap"
+echo -e "  ~/scan_builder2/saved_nmap_scans/$directory/$filename.xml"
+echo -e "  ~/scan_builder2/saved_nmap_scans/$directory/$filename.html"
+echo -e "  ~/scan_builder2/saved_nmap_scans/$directory/$filename.gnmap"
+echo -e "  ~/scan_builder2/saved_nmap_scans/$directory/up_hosts.txt"
+echo -e "  ~/scan_builder2/saved_nmap_scans/$directory/input_list.txt${cn}"
